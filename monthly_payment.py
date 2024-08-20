a3b064110ec5b87ca3fb875650f12d4baa55e872
edddes <edde.ranheden@gmail.com>
2024-08-20 10:46:18 +0200

    first commit

---
+++ monthly_payment.py
@@ -0,0 +1,17 @@
+
+
+def calculate_monthly_payment(principal, annual_rate, years):
+    monthly_rate = annual_rate / 12 / 100
+    months = years*12¨
+    monthly_payment = (principal * monthly_rate) / (1- (1))   
+
+ 
+
+
+def main():
+principal = float(input("Lånebelopp: "))
+annual_rate = float(input("Årsränta (%): "))
+years = int(input("Löptid (år): "))
+payment = calculate_monthly_payment(principal, annual_rate, years)
+print(f"Din månatliga betalning är: {payment:.2f} kr")
+main()
